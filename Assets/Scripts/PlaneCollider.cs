ff666c7e3f445671815064824279901989deb9e9
KornelGM <KORNik_k@wp.pl>
2020-04-27 13:15:37 +0200

    Added confetti for winning a level, 'paper plane ragdoll' and delayed level end

--- Assets/Scripts/PlaneCollider.cs
+++ Assets/Scripts/PlaneCollider.cs
@@ -1,14 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
-public class PlaneCollider : MonoBehaviour {
-	private void OnTriggerEnter(Collider other) {
+public class PlaneCollider : MonoBehaviour
+{
+	public UnityEvent OnWin = null;
+
+	private bool won = false;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if ( won )
+			return;
+
 		if (other.gameObject.tag == "Trashcan") {
-			SceneManager.LoadScene("Win");
+			OnWin?.Invoke( );
+			Invoke( nameof( Win ), 3f );
+			won = true;
 		} else if (other.gameObject.tag != "Player") {
 			SceneManager.LoadScene("Loose");
 		}
+	}
+
+	private void Win()
+	{
+		SceneManager.LoadScene( "Win" );
 	}
 }
