dadfb58f1098fddcea359a1467ceca479fb43a2a
Michael Fewkes <mdfewkes@gmail.com>
2020-05-11 19:40:56 -0600

    Started work on level manager

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public string currentScene;
+	public int currentLevel = 0;
+
+	[SerializeField]
+	string[] findThePailLevels = new string[] { "DemoLevel" };
+
+	void Start() {
+		currentScene = SceneManager.GetActiveScene().name;
+	}
+
+	void Update() {
+
+	}
+}
