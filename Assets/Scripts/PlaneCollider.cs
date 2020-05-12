7ebfec8b8e29afa31bbda3efaf3e00271f9ec8fe
Michael Fewkes <mdfewkes@gmail.com>
2020-05-11 18:37:34 -0600

    Paperplane ragdoll on lose

--- Assets/Scripts/PlaneCollider.cs
+++ Assets/Scripts/PlaneCollider.cs
@@ -4,31 +4,38 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
-public class PlaneCollider : MonoBehaviour
-{
-	public UnityEvent OnWin = null;
+public class PlaneCollider : MonoBehaviour {
 
-	private bool won = false;
+	public UnityEvent OnWin = null;
+	public UnityEvent OnLose = null;
+	public float holdOnWin = 3.0f;
+	public float holdOnLose = 1.5f;
+
+	private bool fall = false;
 
 	private void OnTriggerEnter(Collider other) {
-		if (won) {
+		if (fall) {
 			return;
 		}
 
 		if (other.gameObject.tag == "Trashcan") {
 			OnWin?.Invoke();
-			Invoke(nameof(Win), 3f);
-			won = true;
+			Invoke(nameof(Win), holdOnWin);
+			fall = true;
 		} else if (other.gameObject.tag == "Boost") {
 			gameObject.GetComponent<FlightController>().speed *= 2f;
 			other.gameObject.SetActive(false);
 		} else if (other.gameObject.tag != "Player") {
-			SceneManager.LoadScene("Loose");
+			OnLose?.Invoke();
+			Invoke(nameof(Lose), holdOnLose);
+			fall = true;
 		}
 	}
 
-	private void Win()
-	{
-		SceneManager.LoadScene( "Win" );
+	private void Win() {
+		SceneManager.LoadScene("Win");
+	}
+	private void Lose() {
+		SceneManager.LoadScene("Loose");
 	}
 }
