30dc3dcf201f881a59e7783abb1df9a3322e09b7
Michael Fewkes <mdfewkes@gmail.com>
2020-05-15 12:16:32 -0600

    Smoother camera on crashes

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -4,24 +4,48 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour {
 
+	public bool locked = false;
+
 	float cameraOffsetX = 0f;
 	float cameraOffsetY = 0f;
+	Vector3 oldView = new Vector3();
 
 	void Update() {
+		if (locked) {
+			LockedCamera();
+		} else {
+			FollowCamera();
+		}
+	}
 
-		cameraOffsetX += Input.GetAxis("MouseX")/5;
+	public void SetCameraLock(bool setLock) {
+		locked = setLock;
+	}
+
+	private void FollowCamera() {
+
+		cameraOffsetX += Input.GetAxis("MouseX") / 5;
 		if (cameraOffsetX > 30) { cameraOffsetX = 30; }
 		if (cameraOffsetX < -30) { cameraOffsetX = -30; }
 
-		cameraOffsetY += Input.GetAxis("MouseY")/5;
+		cameraOffsetY += Input.GetAxis("MouseY") / 5;
 		if (cameraOffsetY > 30) { cameraOffsetY = 30; }
 		if (cameraOffsetY < -30) { cameraOffsetY = -30; }
 
 		Vector3 moveCamTo = transform.position - transform.forward * 10f + Vector3.up * 5 - Vector3.up * cameraOffsetY / 4 - transform.right * cameraOffsetX / 4;
 		float bias = 0.95f;
 		Camera.main.transform.position = Camera.main.transform.position * bias + moveCamTo * (1f - bias);
-		Camera.main.transform.LookAt(transform.position + transform.forward * 30f + transform.right * cameraOffsetX + Vector3.up * cameraOffsetY / 3);
-		
+		Vector3 moveViewTo = transform.position + transform.forward * 30f + transform.right * cameraOffsetX + Vector3.up * cameraOffsetY / 3;
+		Camera.main.transform.LookAt(moveViewTo);
+		oldView = moveViewTo;
+
+	}
+
+	private void LockedCamera() {
+		float bias = 0.99f;
+		Vector3 moveViewTo = oldView * bias + transform.position * (1f - bias);
+		Camera.main.transform.LookAt(moveViewTo);
+		oldView = moveViewTo;
 	}
 
 }
