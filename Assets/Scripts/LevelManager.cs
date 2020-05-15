bfab53247384b14854503aa32e73a4d3dd5069b4
Michael Fewkes <mdfewkes@gmail.com>
2020-05-15 11:31:30 -0600

    Level manager more robust

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -11,9 +11,8 @@ public class LevelManager : MonoBehaviour {
 	public string currentScene;
 	public int currentLevel = 0;
 	public GameType currentGameType = GameType.FindThePail;
-
-	[SerializeField]
-	private string[] findThePailLevels = new string[] { "ftpTest1", "ftpTest2", "ftpTest3", "DemoLevel" };
+	
+	private string[][] levels = { new[] { "ftpTest1", "ftpTest2", "ftpTest3" } };
 
 	void Awake() {
 		if (Instance == null) {
@@ -29,16 +28,12 @@ public class LevelManager : MonoBehaviour {
 	}
 
 	public void LoadLevel() {
-
-		switch (currentGameType) {
-		case GameType.FindThePail:
-			if (currentLevel >= findThePailLevels.Length) {
-				currentLevel = 0;
-				SceneManager.LoadScene("Title");
-			} else {
-				SceneManager.LoadScene(findThePailLevels[currentLevel]);
-			}
-			break;
+		
+		if (currentLevel >= levels[(int)currentGameType].Length) {
+			currentLevel = 0;
+			SceneManager.LoadScene("Title");
+		} else {
+			SceneManager.LoadScene(levels[(int)currentGameType][currentLevel]);
 		}
 	}
 
