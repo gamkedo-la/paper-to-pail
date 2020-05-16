45e73b3c101c554fb01e6cd49d6ceda5f0f6fc6e
Michael Fewkes <mdfewkes@gmail.com>
2020-05-15 22:03:44 -0600

    Fan in demo room.  Disabled direct zones while I find a better solution

--- Assets/Scripts/PlaneCollider.cs
+++ Assets/Scripts/PlaneCollider.cs
@@ -15,7 +15,7 @@ public class PlaneCollider : MonoBehaviour {
 	private bool fall = false;
 
 	private Vector3 pushPlane = new Vector3();
-	private float pushMultiplier = 20;
+	private float pushMultiplier = 10;
 
 	private Vector3 directPlane = new Vector3();
 	private float directMultiplier = 0.2f;
@@ -24,8 +24,7 @@ public class PlaneCollider : MonoBehaviour {
 
 	void Update() {
 		transform.position += pushPlane * pushMultiplier * Time.deltaTime;
-		transform.Rotate(directPlane * directMultiplier * Time.deltaTime);
-		
+		//transform.Rotate(directPlane * directMultiplier * Time.deltaTime);
 	}
 
 	void OnTriggerEnter(Collider other) {
@@ -37,16 +36,17 @@ public class PlaneCollider : MonoBehaviour {
 			OnWin?.Invoke();
 			Invoke(nameof(Win), holdOnWin);
 			fall = true;
-		} else if (other.gameObject.tag == "PushZone") {
+		} else if (other.gameObject.CompareTag("PushZone")) {
 			Debug.Log("Enter Push");
 			pushPlane += other.gameObject.transform.forward;
-		} else if (other.gameObject.tag == "DirectZone") {
+		} else if (other.gameObject.CompareTag("DirectZone")) {
 			Debug.Log("Enter Direct");
 			directPlane += other.gameObject.transform.rotation.eulerAngles;
-		} else if (other.gameObject.tag == "Boost") {
+			Debug.Log(directPlane);
+		} else if (other.gameObject.CompareTag("Boost")) {
 			gameObject.GetComponent<FlightController>().speed *= 1.5f;
 			other.gameObject.SetActive(false);
-		} else if (other.gameObject.tag != "Player") {
+		} else if (!other.gameObject.CompareTag("Player")) {
 			OnLose?.Invoke();
 			Invoke(nameof(Lose), holdOnLose);
 			fall = true;
@@ -54,12 +54,13 @@ public class PlaneCollider : MonoBehaviour {
 	}
 
 	void OnTriggerExit(Collider other) {
-		if (other.gameObject.tag == "PushZone") {
+		if (other.gameObject.CompareTag("PushZone")) {
 			Debug.Log("Exit Push");
 			pushPlane -= other.gameObject.transform.forward;
-		} else if (other.gameObject.tag == "DirectZone") {
+		} else if (other.gameObject.CompareTag("DirectZone")) {
 			Debug.Log("Exit Direct");
-			directPlane -= other.gameObject.transform.rotation.eulerAngles;
+			directPlane -= other.gameObject.transform.localRotation.eulerAngles;
+			Debug.Log(directPlane);
 		}
 	}
 
