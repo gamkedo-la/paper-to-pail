aa4dcd5ebb5782ab0eaf35fdcfdfa7ee4fd9866f
Michael Fewkes <mdfewkes@gmail.com>
2020-06-13 16:05:23 -0600

    Win effect seperates from plane

---
+++ Assets/Scripts/SeperateOnAwake.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeperateOnAwake : MonoBehaviour
+{
+	private void Awake() {
+		transform.SetParent(null);
+	}
+}
