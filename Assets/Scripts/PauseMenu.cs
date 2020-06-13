32c672d1d304ae9b29e1d11f20ac3de0c5ab0d39
brnielsen <47045697+brnielsen@users.noreply.github.com>
2020-06-13 16:48:43 -0400

    Simple Pause Menu (Escape Key and "Options" button on PS4 controller)

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    private bool gamePaused = false;
+
+    [SerializeField]
+    private GameObject pauseMenuCanvas;
+
+    private void Start()
+    {
+        pauseMenuCanvas.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            TogglePause();
+        }
+    }
+
+    public void TogglePause()
+    {
+        if (gamePaused)
+        {
+            Resume();
+        }
+        else
+        {
+            Pause();
+        }
+    }
+
+    private void Pause()
+    {
+        gamePaused = true;
+        pauseMenuCanvas.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    private void Resume()
+    {
+        gamePaused = false;
+        pauseMenuCanvas.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
