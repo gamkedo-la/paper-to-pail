732aa09f6910b6f00adfd08d8eb0f705e61958da
Tyler Funk <tcfunk25@gmail.com>
2020-07-02 22:35:55 -0500

    Fix camera jitter during wall correction

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -13,7 +13,7 @@ public class CameraControl : MonoBehaviour {
 	float cameraOffsetY = 0f;
 	Vector3 oldView = new Vector3();
 
-	void Update() {
+	void LateUpdate() {
 		if (locked) {
 			LockedCamera();
 		} else {
@@ -48,17 +48,17 @@ public class CameraControl : MonoBehaviour {
 		if (cameraOffsetY < -30) { cameraOffsetY = -30; }
 
 		Vector3 moveCamTo = transform.position - transform.forward * 10f + Vector3.up * 5 - Vector3.up * cameraOffsetY / 4 - transform.right * cameraOffsetX / 4;
-		float bias = 0.95f;
-		var targetPosition = Camera.main.transform.position * bias + moveCamTo * (1f - bias);
-		
+		var smooth = 4f;
+
 		// Try not to look through walls.
 		RaycastHit wallHit;
-		if (Physics.Linecast(transform.position, targetPosition, out wallHit, CamOcclusion))
+		if (Physics.Linecast(transform.position, moveCamTo, out wallHit, CamOcclusion))
 		{
-			targetPosition = new Vector3(wallHit.point.x + wallHit.normal.x * 0.5f, targetPosition.y, wallHit.point.z + wallHit.normal.z * 0.5f);
+			smooth = 8f;
+			moveCamTo = new Vector3(wallHit.point.x + wallHit.normal.x * Camera.main.nearClipPlane, moveCamTo.y, wallHit.point.z + wallHit.normal.z * Camera.main.nearClipPlane);
 		}
 
-		Camera.main.transform.position = targetPosition;
+		Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, moveCamTo, Time.deltaTime * smooth);
 		Vector3 moveViewTo = transform.position + transform.forward * 30f + transform.right * cameraOffsetX + Vector3.up * cameraOffsetY / 3;
 		Camera.main.transform.LookAt(moveViewTo);
 		oldView = moveViewTo;
