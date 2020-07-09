d12fb7e4fa634c81cf7685c5fef1e7aa1c1823dc
KornelGM <KORNik_k@wp.pl>
2020-07-09 09:25:03 +0200

    Added randomized encouragement message on the Lose screen (as suggested by McFunkypants)

---
+++ Assets/Scripts/Quote.cs
@@ -0,0 +1,26 @@
+﻿/**
+ * Description: Sets a random quote on enabled.
+ * Authors: Kornel
+ * Copyright: © 2020 Kornel. All rights reserved. For license see: 'LICENSE.txt'
+ **/
+
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+public class Quote : MonoBehaviour
+{
+	[SerializeField] private Text _label = null;
+	[SerializeField] private string[] _quotes = null;
+
+	void Start( )
+	{
+		Assert.IsNotNull( _label, $"Please assign <b>{nameof( _label )}</b> field on <b>{GetType( ).Name}</b> script on <b>{name}</b> object" );
+		Assert.AreNotEqual( _quotes.Length, 0, $"Please assign <b>{nameof( _quotes )}</b> field on <b>{GetType( ).Name}</b> script on <b>{name}</b> object" );
+	}
+
+	void OnEnable( )
+	{
+		_label.text = _quotes[Random.Range( 0, _quotes.Length )];
+	}
+}
