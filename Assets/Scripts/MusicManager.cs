e69649f03c109b0d53e78dc55679bfdc0d43c6c4
Michael Fewkes <mdfewkes@gmail.com>
2020-07-14 19:45:45 -0600

    More robust music manager

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class MusicManager : MonoBehaviour {
 
@@ -9,9 +10,12 @@ public class MusicManager : MonoBehaviour {
 	public AudioClip scheduledTrack;
 
 	public AudioSource currentTrack;
-	public float endTime;
-	public bool manualLoop;
+	public double timeNow;
+	public double startTime;
+	public double endTime;
 
+	public bool manualLoop = false;
+	public bool nothingSchedualled = false;
 	public float fadeTime = 0.25f;
 	public bool fadeOnEnd = true;
 
@@ -25,15 +29,18 @@ public class MusicManager : MonoBehaviour {
 	}
 
 	void Update() {
+		timeNow = AudioSettings.dspTime;
 
-		if (currentTrack != null && manualLoop && scheduledTrack != null && endTime <= currentTrack.time) {
-			MusicManager.Instance.PlayTrack(scheduledTrack, false);
+		if (currentTrack != null && scheduledTrack != null && manualLoop && nothingSchedualled && endTime <= timeNow-fadeTime) { //Manual loop and schedualled track
+			PlayTrackSchedualled(scheduledTrack, endTime, false);
 			scheduledTrack = null;
-		} else if (currentTrack != null && scheduledTrack != null && endTime <= currentTrack.time) {
-			MusicManager.Instance.PlayTrack(scheduledTrack, true);
+			nothingSchedualled = false;
+		} else if (currentTrack != null && scheduledTrack != null && endTime <= timeNow) { //Autolooping and schedualled track
+			PlayTrack(scheduledTrack, true);
 			scheduledTrack = null;
-		} else if (currentTrack != null && manualLoop && endTime <= currentTrack.time) {
-			MusicManager.Instance.PlayTrack(currentTrack.clip, endTime, false);
+			nothingSchedualled = false;
+		} else if (currentTrack != null && manualLoop && endTime <= timeNow) { //Manual looping, none schedualled
+			PlayTrack(currentTrack.clip, endTime, false);
 		}
 	}
 
@@ -48,14 +55,15 @@ public class MusicManager : MonoBehaviour {
 		currentTrack.loop = true;
 		currentTrack.gameObject.name = "Music Track";
 
-		endTime = currentTrack.clip.length;
+		startTime = AudioSettings.dspTime;
+		endTime = startTime + currentTrack.clip.length;
 		manualLoop = false;
 		fadeOnEnd = fadeOut;
 
 		currentTrack.Play();
 	}
 
-	public void PlayTrack(AudioClip newTrack, float trackEndTime, bool fadeOut = true) {
+	public void PlayTrack(AudioClip newTrack, double trackEndTime, bool fadeOut = true) {
 		if (currentTrack != null && fadeOnEnd) {
 			StartCoroutine(FadeOutAndStop(currentTrack, fadeTime));
 			currentTrack.gameObject.name = currentTrack.gameObject.name + "(old)";
@@ -66,18 +74,43 @@ public class MusicManager : MonoBehaviour {
 		currentTrack.loop = false;
 		currentTrack.gameObject.name = "Music Track";
 
-		endTime = trackEndTime;
+		startTime = AudioSettings.dspTime;
+		endTime = startTime + trackEndTime;
 		manualLoop = true;
 		fadeOnEnd = fadeOut;
 
 		currentTrack.Play();
 	}
 
+	private void PlayTrackSchedualled(AudioClip newTrack, double schedual, bool fadeOut = false) {
+		if (currentTrack != null && fadeOnEnd) {
+			StartCoroutine(FadeOutAndStop(currentTrack, fadeTime));
+			currentTrack.gameObject.name = currentTrack.gameObject.name + "(old)";
+		} else if (currentTrack != null) {
+			currentTrack.gameObject.name = currentTrack.gameObject.name + "(old)";
+		}
+
+
+		currentTrack = Instantiate(musicSourcePrefab).GetComponent<AudioSource>();
+		currentTrack.gameObject.transform.parent = gameObject.transform;
+		currentTrack.clip = newTrack;
+		currentTrack.loop = false;
+		currentTrack.gameObject.name = "Music Track";
+
+		startTime = AudioSettings.dspTime;
+		endTime = startTime + currentTrack.clip.length;
+		manualLoop = true;
+		fadeOnEnd = fadeOut;
+
+		currentTrack.PlayScheduled(schedual);
+		nothingSchedualled = true;
+	}
+
 	public void ScheduleTrack(AudioClip newTrack) {
 		if (currentTrack != null) {
 			scheduledTrack = newTrack;
 		} else {
-			MusicManager.Instance.PlayTrack(scheduledTrack);
+			PlayTrack(scheduledTrack);
 		}
 	}
 
